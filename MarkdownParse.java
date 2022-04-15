018ca8969b279392fd554d5908743515979ebf11
bsalvania <bsalvania@ucsd.edu>
2022-04-15 13:13:28 -0700

    Infinite loop for 4th link
    Exception in thread "main" java.lang.OutOfMemoryError: Java heap space
            at java.base/java.util.Arrays.copyOfRange(Arrays.java:3822)
            at java.base/java.lang.StringLatin1.newString(StringLatin1.java:769)
            at java.base/java.lang.String.substring(String.java:2709)
            at MarkdownParse.getLinks(MarkdownParse.java:21)
            at MarkdownParse.main(MarkdownParse.java:34)

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -11,15 +11,19 @@ public class MarkdownParse {
         ArrayList<String> toReturn = new ArrayList<>();
         // find the next [, then find the ], then find the (, then read link upto next )
         int currentIndex = 0;
+
+
         while(currentIndex < markdown.length()) {
             int openBracket = markdown.indexOf("[", currentIndex);
             int closeBracket = markdown.indexOf("]", openBracket);
             int openParen = markdown.indexOf("(", closeBracket);
             int closeParen = markdown.indexOf(")", openParen);
             toReturn.add(markdown.substring(openParen + 1, closeParen));
+            //System.out.println("Current: " + currentIndex);
             currentIndex = closeParen + 1;
-        }
+            //System.out.println("After: " + currentIndex);
 
+        }
         return toReturn;
     }
 
@@ -31,3 +35,4 @@ public class MarkdownParse {
 	    System.out.println(links);
     }
 }
+
