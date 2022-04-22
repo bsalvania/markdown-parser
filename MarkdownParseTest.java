b862b7641e5bf427074ff1d0e1bfd5d142df2807
bsalvania <bsalvania@ucsd.edu>
2022-04-22 12:26:29 -0700

    Update MarkdownParseTest.java
    javac -cp ".;lib\junit-4.13.2.jar;lib\hamcrest-core-1.3.jar" MarkdownParseTest.java
    
    java -cp ".;lib/junit-4.13.2.jar;lib/hamcrest-core-1.3.jar" org.junit.runner.JUnitCore MarkdownParseTest

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -1,10 +1,10 @@
-import static org.junit.Assert.*;
-import org.junit.*;
-public class MarkdownParseTest {
-
-    @Test
-    public void addition() {
-        assertEquals(2, 1 + 1);
+import static org.junit.Assert.*;  //Imports assert method
+import org.junit.*; //Imports jUnit for compiler to understand the tests
+public class MarkdownParseTest { //Creates a class to create tests
+    @Test                       //Lets compiler know that this is a jUnit test
+    public void addition() {    //Method to test an addition problem
+        assertEquals(2, 1 + 1); //Checks if the actual result (right) matches the expected result (left)
     }
-
 }
+
+
