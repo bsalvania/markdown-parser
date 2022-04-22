d59e4afd01e7386aa1ab32f20122f1f97f31b748
bsalvania <bsalvania@ucsd.edu>
2022-04-22 11:40:54 -0700

    Infinite loop fix
    If a link has another set of parenthesis inside it, the function would enter an infinite loop. Now it knows where to correctly stop when adding the link and successfully adds the link to the array list.

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -12,20 +12,30 @@ public class MarkdownParse {
         // find the next [, then find the ], then find the (, then read link upto next )
         int currentIndex = 0;
 
-
         while(currentIndex < markdown.length()) {
             int openBracket = markdown.indexOf("[", currentIndex);
             int closeBracket = markdown.indexOf("]", openBracket);
             int openParen = markdown.indexOf("(", closeBracket);
             int closeParen = markdown.indexOf(")", openParen);
-            //int closeParen = markdown.lastIndexOf(")", openParen);
-            toReturn.add(markdown.substring(openParen + 1, closeParen));
-            currentIndex = closeParen + 1;
+            int openParen2 = markdown.indexOf("(", openParen + 1);
+            int closeParen2 = markdown.indexOf(")", closeParen + 1);
 
+            //If statement checks if there is another open parenthese before
+            //the end of the current line, and if so, go to the 2nd close
+            //parenthese 
+            if (openParen2 != -1 && closeParen > openParen2)
+            {
+                toReturn.add(markdown.substring(openParen + 1, closeParen2));
+                currentIndex = closeParen2 + 1;
+            }
+            else 
+            {
+                toReturn.add(markdown.substring(openParen + 1, closeParen));
+                currentIndex = closeParen + 1;
+            }
         }
         return toReturn;
     }
-
 
     public static void main(String[] args) throws IOException {
         Path fileName = Path.of(args[0]);
