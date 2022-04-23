009dc08edea5189ff9e25b9e8e1806525762b918
bsalvania <bsalvania@ucsd.edu>
2022-04-23 14:48:33 -0700

    Just testing for report 2

--- Test2.java
+++ Test2.java
@@ -5,45 +5,24 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
 
-public class MarkdownParse {
+public class Test2 {
 
     public static ArrayList<String> getLinks(String markdown) {
         ArrayList<String> toReturn = new ArrayList<>();
         // find the next [, then find the ], then find the (, then read link upto next )
         int currentIndex = 0;
-
         while(currentIndex < markdown.length()) {
             int openBracket = markdown.indexOf("[", currentIndex);
             int closeBracket = markdown.indexOf("]", openBracket);
             int openParen = markdown.indexOf("(", closeBracket);
             int closeParen = markdown.indexOf(")", openParen);
-            int openParen2 = markdown.indexOf("(", openParen + 1);
-            int closeParen2 = markdown.indexOf(")", closeParen + 1);
-
-            //If statement checks if there is another open parentheses before
-            //the end of the current line, and if so, go to the 2nd close
-            //parenthese 
-            if (openParen2 != -1 && closeParen > openParen2)
-            {
-                toReturn.add(markdown.substring(openParen + 1, closeParen2));
-                currentIndex = closeParen2 + 1;
-            }
-            else if (markdown.charAt(openBracket-1)== '!') 
-            {
-                currentIndex = closeParen + 1;
-            }
-            else if (openParen == -1)
-            {
-                return toReturn;
-            }
-            else 
-            {
-                toReturn.add(markdown.substring(openParen + 1, closeParen));
-                currentIndex = closeParen + 1;
-            }
+            toReturn.add(markdown.substring(openParen + 1, closeParen));
+            currentIndex = closeParen + 1;
         }
+
         return toReturn;
     }
+
 
     public static void main(String[] args) throws IOException {
         Path fileName = Path.of(args[0]);
@@ -52,4 +31,3 @@ public class MarkdownParse {
 	    System.out.println(links);
     }
 }
-
