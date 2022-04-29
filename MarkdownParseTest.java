7f64093670e04327c854a7022df1504520c0d33d
bsalvania <bsalvania@ucsd.edu>
2022-04-29 13:38:30 -0700

    Make a test that fails
    Multiplication should fail

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -40,7 +40,7 @@ public class MarkdownParseTest { //Creates a class to create tests
 
     @Test                       
     public void multiplicationFail() {
-        assertEquals(8, 2 * 3); 
+        assertEquals(8, 2 * 5); 
     }
 
     @Test
