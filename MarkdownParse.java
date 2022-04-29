23074c0599622c9e4dcf26011dab66787eb431dd
bsalvania <bsalvania@ucsd.edu>
2022-04-29 12:56:00 -0700

    Change to Joseph's MarkdownParse

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -11,39 +11,33 @@ public class MarkdownParse {
         ArrayList<String> toReturn = new ArrayList<>();
         // find the next [, then find the ], then find the (, then read link upto next )
         int currentIndex = 0;
-
         while(currentIndex < markdown.length()) {
             int openBracket = markdown.indexOf("[", currentIndex);
+            //No links left to be found exit
+            if (openBracket == -1) {
+                break;
+            }
             int closeBracket = markdown.indexOf("]", openBracket);
             int openParen = markdown.indexOf("(", closeBracket);
-            int closeParen = markdown.indexOf(")", openParen);
-            int openParen2 = markdown.indexOf("(", openParen + 1);
-            int closeParen2 = markdown.indexOf(")", closeParen + 1);
-
-            //If statement checks if there is another open parentheses before
-            //the end of the current line, and if so, go to the 2nd close
-            //parentheses
-            if (openParen2 != -1 && closeParen > openParen2)
-            {
-                toReturn.add(markdown.substring(openParen + 1, closeParen2));
-                currentIndex = closeParen2 + 1;
+            //no close paren so there is no link, ensures no infinite loop
+            if (openParen == -1) {
+                break;
             }
-            else if (markdown.charAt(openBracket-1)== '!') 
-            {
+            int closeParen = markdown.indexOf(")", openParen);
+            if (markdown.charAt(openBracket-1)== '!' || openParen != closeBracket-1) {
                 currentIndex = closeParen + 1;
             }
-            else if (openParen == -1)
-            {
-                return toReturn;
-            }
-            else 
-            {
+            else {
                 toReturn.add(markdown.substring(openParen + 1, closeParen));
                 currentIndex = closeParen + 1;
             }
         }
+        if (toReturn.size() == 0) {
+            System.out.println("No links found");
+        }
         return toReturn;
     }
+
 
     public static void main(String[] args) throws IOException {
         Path fileName = Path.of(args[0]);
@@ -52,4 +46,3 @@ public class MarkdownParse {
 	    System.out.println(links);
     }
 }
-
