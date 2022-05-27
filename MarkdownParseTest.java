852365e53295b2ec6eb87424fb315cfc70224c44
bsalvania <bsalvania@ucsd.edu>
2022-05-27 15:06:42 -0700

    Update MarkdownParseTest.java

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -109,5 +109,74 @@ public class MarkdownParseTest { //Creates a class to create tests
         }
     }
 
+    @Test
+    public void testSnippet1() throws IOException {
+        ArrayList<String> snippet1ExpectedLinks = new ArrayList<>();
+        snippet1ExpectedLinks.add("url.com");
+        snippet1ExpectedLinks.add("google.com");
+        snippet1ExpectedLinks.add("google.com");
+        snippet1ExpectedLinks.add("ucsd.edu");
+
+
+        Path fileName = Paths.get("snippet1.md");
+        try {
+            String content = Files.readString(fileName); 
+            ArrayList<String> links = MarkdownParse.getLinks(content);
+
+            for (int i = 0; i < links.size(); i++)
+            {
+                assertEquals(snippet1ExpectedLinks.get(i), links.get(i));
+            }
+        } catch (IOException e) {
+            System.out.println("Exception thrown");
+        }
+    }
+
+    @Test
+    public void testSnippet2() throws IOException {
+        ArrayList<String> snippet2ExpectedLinks = new ArrayList<>();
+        snippet2ExpectedLinks.add("a.com");
+        snippet2ExpectedLinks.add("b.com");
+        snippet2ExpectedLinks.add("a.com(())");
+        snippet2ExpectedLinks.add("example.com");
+
+
+        Path fileName = Paths.get("snippet2.md");
+        try {
+            String content = Files.readString(fileName); 
+            ArrayList<String> links = MarkdownParse.getLinks(content);
+
+            for (int i = 0; i < links.size(); i++)
+            {
+                assertEquals(snippet2ExpectedLinks.get(i), links.get(i));
+            }
+        } catch (IOException e) {
+            System.out.println("Exception thrown");
+        }
+    }
+
+    @Test
+    public void testSnippet3() throws IOException {
+        ArrayList<String> snippet3ExpectedLinks = new ArrayList<>();
+        snippet3ExpectedLinks.add("https://www.twitter.com");
+        snippet3ExpectedLinks.add("https://sites.google.com/eng.ucsd.edu/cse-15l-spring-2022/schedule");
+        snippet3ExpectedLinks.add("github.com");
+        snippet3ExpectedLinks.add("https://cse.ucsd.edu/");
+
+
+        Path fileName = Paths.get("snippet1.md");
+        try {
+            String content = Files.readString(fileName); 
+            ArrayList<String> links = MarkdownParse.getLinks(content);
+
+            for (int i = 0; i < links.size(); i++)
+            {
+                assertEquals(snippet3ExpectedLinks.get(i), links.get(i));
+            }
+        } catch (IOException e) {
+            System.out.println("Exception thrown");
+        }
+    }
+
 
 }
